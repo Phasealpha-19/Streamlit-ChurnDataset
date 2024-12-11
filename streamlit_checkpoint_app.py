f818c514b31e368c5c0a04af19a47c9cbdb9dfd9
Olufemi Olagbaju <111253424+Phasealpha-19@users.noreply.github.com>
2024-12-11 13:28:01 +0100

    Add files via upload
    it wasnt the final commit

--- streamlit_checkpoint_app.py
+++ streamlit_checkpoint_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import joblib
 import numpy as np
 
-cols = ['REGION_No Region', 'REGULARITY', 'TOP_PACK', 'REGION_DAKAR']
+cols = ['REGION_No Region', 'REGULARITY', 'TOP_PACK', 'REGION_DAKAR', 'REGION_THIES', 'REGION_SAINT-LOUIS']
 
 reg = st.number_input("Enter Regularity", min_value = 0.0 )
 
@@ -17,11 +17,15 @@ with open("my_log_model.pkl", "rb") as file:
     my_model = joblib.load(file)
 
 if selected_region  == "No Region":
-    prediction = my_model.predict(np.array([1, reg, top_pack, 0]).reshape(1,-1))
+    prediction = my_model.predict(np.array([1, reg, top_pack, 0, 0, 0]).reshape(1,-1))
 elif selected_region == "DAKAR":
-    prediction = my_model.predict(np.array([0, reg, top_pack, 1]).reshape(1,-1))
+    prediction = my_model.predict(np.array([0, reg, top_pack, 1, 0, 0]).reshape(1,-1))
+elif selected_region == "THIES":
+    prediction = my_model.predict(np.array([0, reg, top_pack, 0, 1, 0]).reshape(1,-1))
+elif selected_region == "SAINT-LOUIS":
+    prediction = my_model.predict(np.array([0, reg, top_pack, 0, 0, 1]).reshape(1,-1))
 else:
-    prediction = my_model.predict(np.array([0, reg, top_pack, 0]).reshape(1,-1))
+    prediction = my_model.predict(np.array([0, reg, top_pack, 0, 0, 0]).reshape(1,-1))
 
 
 if reg and selected_region and top_pack:
